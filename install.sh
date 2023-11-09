1196e492960badcb704d84e023578f6690931d6d
Brett Josef C. Galvez <98447511+suniason@users.noreply.github.com>
2023-11-09 17:10:24 +0000

    for saving purposes

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ sh -c "$(curl -sSfL https://release.solana.com/v1.17.3/install)"
 # Install Rust and Cargo
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 
-cargo install --git https://github.com/coral-xyz/anchor avm --locked --force
 
 echo "Solana, Anchor and Rust are installed!"
 
@@ -13,9 +12,10 @@ npm install
 npm install -g tsx ts-node
 
 # Run this commands after
-# export PATH="/home/codespace/.local/share/solana/install/active_release/bin:$PATH"
-# source "$HOME/.cargo/env"
+export PATH="/home/codespace/.local/share/solana/install/active_release/bin:$PATH"
+source "$HOME/.cargo/env"
 
+cargo install --git https://github.com/coral-xyz/anchor avm --locked --force
 # Try this command to check
 # solana --version
 # cargo --version
--- programs/recipe-anchor-app/src/lib.rs
+++ programs/recipe-anchor-app/src/lib.rs
@@ -13,3 +13,26 @@ pub mod recipe_anchor_app {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+pub struct CreateRecipe<'info> {
+    
+    #[account(init, payer = owner, space = 8 + 1 + 50 + 32)]
+    pub recipes : Account<'info, Recipe>,
+    pub owner : Signer<'info>,
+    pub system_program : Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct GetAllRecipes<'info> {
+    pub recipe: Account<'info, Recipe>,
+    pub recipe_collection: Data<'info, Vec<Recipe>>,
+}
+
+#[account]
+pub struct Recipe {
+    pub author : Pubkey,
+    pub name : String,
+    pub ingredients : Vec<String>, 
+    pub equipments : Vec<String>, 
+    pub procedure : String, 
+}
