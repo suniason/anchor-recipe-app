77ced975b4f2bd77ccad65c635879eb87df5be72
Brett Josef C. Galvez <98447511+suniason@users.noreply.github.com>
2023-11-09 16:32:56 +0000

    added install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+# Install Solana Toolsuite
+sh -c "$(curl -sSfL https://release.solana.com/v1.17.3/install)"
+
+# Install Rust and Cargo
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+
+cargo install --git https://github.com/coral-xyz/anchor avm --locked --force
+
+echo "Solana, Anchor and Rust are installed!"
+
+# Install node dependencies
+npm install
+npm install -g tsx ts-node
+
+# Run this commands after
+# export PATH="/home/codespace/.local/share/solana/install/active_release/bin:$PATH"
+# source "$HOME/.cargo/env"
+
+# Try this command to check
+# solana --version
+# cargo --version
