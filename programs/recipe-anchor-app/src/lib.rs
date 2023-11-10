20f4fbb4e0b00dce33d78123ee58e04d1b7afb24
Brett Josef C. Galvez <98447511+suniason@users.noreply.github.com>
2023-11-10 09:44:25 +0000

    added frontend

--- programs/recipe-anchor-app/src/lib.rs
+++ programs/recipe-anchor-app/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
 
 declare_id!("6xpX3GHGBP7JcEAMuGhhFLxQy16RMESnNjnJteiSHu8X");
 
@@ -6,33 +7,37 @@ declare_id!("6xpX3GHGBP7JcEAMuGhhFLxQy16RMESnNjnJteiSHu8X");
 pub mod recipe_anchor_app {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn create_recipe(ctx: Context<CreateRecipe>, 
+        name:String, 
+        ingredients: String, 
+        equipments: String, 
+        procedure: String) -> Result<()> {
+
+        let recipe_account = &mut ctx.accounts.recipe;
+        recipe_account.author = ctx.accounts.signer.key();
+        recipe_account.name = name;
+        recipe_account.ingredients = ingredients;
+        recipe_account.equipments = equipments;
+        recipe_account.procedure = procedure;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
-
 pub struct CreateRecipe<'info> {
-    
-    #[account(init, payer = owner, space = 8 + 1 + 50 + 32)]
-    pub recipes : Account<'info, Recipe>,
+    #[account(init, payer = owner, space =  size_of::<Recipe>() + 256)]
+    pub recipe : Account<'info, Recipe>,
+    #[account(mut)]
     pub owner : Signer<'info>,
     pub system_program : Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct GetAllRecipes<'info> {
-    pub recipe: Account<'info, Recipe>,
-    pub recipe_collection: Data<'info, Vec<Recipe>>,
 }
 
 #[account]
 pub struct Recipe {
     pub author : Pubkey,
     pub name : String,
-    pub ingredients : Vec<String>, 
-    pub equipments : Vec<String>, 
+    pub ingredients : String, 
+    pub equipments : String, 
     pub procedure : String, 
 }
--- tests/recipe-anchor-app.ts
+++ tests/recipe-anchor-app.ts
@@ -1,16 +1,44 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { RecipeAnchorApp } from "../target/types/recipe_anchor_app";
+import { assert } from "chai";
 
 describe("recipe-anchor-app", () => {
-  // Configure the client to use the local cluster.
+
   anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.getProvider();
   const program = anchor.workspace.RecipeAnchorApp as Program<RecipeAnchorApp>;
+  const keyPair = anchor.web3.Keypair.generate();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Should create customer", async () => {
+    const name: string = "Chicken Adobo";
+    const ingredients: string = `2 lbs chicken thighs, 1 cup soy sauce, 
+    1 cup white vinegar, 1 head garlic, minced, 
+    1 tsp peppercorns, 3 bay leaves, 1 tbsp cooking oil`
+    const equipments: string = 'Large pot, Cooking spoon, Knife, Chopping board';
+    const procedure: string = `
+      1. Marinate chicken in soy sauce, vinegar, garlic, and peppercorns for 30 minutes.
+      2. Brown marinated chicken in oil in a large pot.
+      3. Add bay leaves and the marinade, simmer for 30-40 minutes until chicken is tender.
+      4. Serve hot over rice. Enjoy your Chicken Adobo!
+    `;
+
+    await program.methods
+      .createRecipe(provider.publicKey, name, ingredients, equipments, procedure)
+      .accounts({
+        recipe: keyPair.publicKey,
+        owner: provider.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([keyPair])
+      .rpc();
+
+    const recipe = await program.account.recipe.fetch(keyPair.publicKey);
+    assert.ok(recipe.author === provider.publicKey);
+    assert.ok(recipe.name === name);
+    assert.ok(recipe.ingredients === ingredients);
+    assert.ok(recipe.equipments === equipments);
+    assert.ok(recipe.procedure === procedure);
   });
-});
+
+}
